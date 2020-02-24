e565c62dc06b93073cac1b528880b44f0af3fc90
akash s arjun <akashsarjun@cusat.ac.in>
2020-02-24 22:21:44 +0100

    implement Picture in Picture(PIP)

--- app/src/main/java/com/aksar/android_pip/MainActivity.java
+++ app/src/main/java/com/aksar/android_pip/MainActivity.java
@@ -1,14 +1,48 @@
 package com.aksar.android_pip;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.app.PictureInPictureParams;
+import android.graphics.Point;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Rational;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        actionBar = getActionBar();
+        button = findViewById(R.id.enter_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View view) {
+                Display display = getWindowManager().getDefaultDisplay();
+                Point point = new Point();
+
+                display.getSize(point);
+                int width = point.x;
+                int height = point.y;
+
+                Rational rational = new Rational(width, height);
+                PictureInPictureParams.Builder pip_Builder = new PictureInPictureParams.Builder();
+                pip_Builder.setAspectRatio(rational).build();
+                enterPictureInPictureMode(pip_Builder.build());
+            }
+        });
+
     }
 }
